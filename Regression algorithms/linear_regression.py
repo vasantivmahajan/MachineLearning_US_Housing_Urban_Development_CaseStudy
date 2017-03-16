86a7a8610e076ea720fd3dc1b7c08f1a361da8df
vasantivmahajan <mahajan.v@husky.neu.edu>
2017-03-16 18:58:14 -0400

    KNN upadted

--- Regression algorithms/linear_regression.py
+++ Regression algorithms/linear_regression.py
@@ -8,8 +8,12 @@ from sklearn.preprocessing import StandardScaler
 #pip install scikit-neuralnetwork 
 #conda update scikit-learn
 from sklearn.neural_network import MLPClassifier
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.cross_validation import train_test_split, KFold, StratifiedShuffleSplit,StratifiedKFold
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_squared_error
+from sklearn.utils import check_array
+from sklearn.metrics import r2_score
 
 """dataset=pd.DataFrame("historical_data1_Q12005.csv")
 print(dataset.head(10))
@@ -26,6 +30,15 @@ print(regr.coef_)
 np.mean((regr.predict(X_test)-Y_test)**2)
 regr.score(X_test, Y_test) """
 
+def mean_absolute_percentage_error(y_true, y_pred): 
+    y_true, y_pred = check_array(y_true, y_pred)
+
+    ## Note: does not handle mix 1d representation
+    #if _is_1d(y_true): 
+    #    y_true, y_pred = _check_1d_array(y_true, y_pred)
+
+    return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
+
 #using label encoder to convert categorical columns into numeric values
 def dummyEncode(df):
         columnsToEncode = list(df.select_dtypes(include=['category','object']))
@@ -39,18 +52,18 @@ def dummyEncode(df):
 
 def compute_coefficients(dataframe):
         #using statsmodel estimate the model coefficients for predictions
-        credit_score = smf.ols(formula='Original_interest_rate~Credit_score', data=dataframe).fit()
-        combined_loan_to_value = smf.ols(formula='Original_interest_rate~Original_combined_loan_to_value', data=dataframe).fit()
-        Original_debt_to_income_ratio=smf.ols(formula='Original_interest_rate~Original_debt_to_income_ratio', data=dataframe).fit()
-        Original_loan_to_value=smf.ols(formula='Original_interest_rate~Original_loan_to_value', data=dataframe).fit()
-        Unpaid_principal_balance=smf.ols(formula='Original_interest_rate~Unpaid_principal_balance', data=dataframe).fit()
-        Property_state=smf.ols(formula='Original_interest_rate~Property_state', data=dataframe).fit()
-        Property_type=smf.ols(formula='Original_interest_rate~Property_type', data=dataframe).fit()
-        Loan_purpose=smf.ols(formula='Original_interest_rate~Loan_purpose', data=dataframe).fit()
-        Original_Loan_Term=smf.ols(formula='Original_interest_rate~Original_Loan_Term', data=dataframe).fit()
-        Number_of_borrowers=smf.ols(formula='Original_interest_rate~Number_of_borrowers', data=dataframe).fit()
-        Seller_name=smf.ols(formula='Original_interest_rate~Seller_name', data=dataframe).fit()
-        Servicer_name=smf.ols(formula='Original_interest_rate~Servicer_name', data=dataframe).fit()
+        credit_score = smf.ols(formula='ORIGINAL_INTEREST_RATE~CREDIT_SCORE', data=dataframe).fit()
+        combined_loan_to_value = smf.ols(formula='ORIGINAL_INTEREST_RATE~ORGINAL_COMBINED_LOAN_TO_VALUE', data=dataframe).fit()
+        Original_debt_to_income_ratio=smf.ols(formula='-~ORIGINAL_DEBT_TO_INCOME_RATIO', data=dataframe).fit()
+        Original_loan_to_value=smf.ols(formula='ORIGINAL_INTEREST_RATE~ORIGINAL_LOAN_TO_VALUE', data=dataframe).fit()
+        Unpaid_principal_balance=smf.ols(formula='ORIGINAL_INTEREST_RATE~ORIGINAL_UPB', data=dataframe).fit()
+        Property_state=smf.ols(formula='ORIGINAL_INTEREST_RATE~PROPERTY_STATE', data=dataframe).fit()
+        Property_type=smf.ols(formula='ORIGINAL_INTEREST_RATE~PROPERTY_TYPE', data=dataframe).fit()
+        Loan_purpose=smf.ols(formula='ORIGINAL_INTEREST_RATE~LOAN_PURPOSE', data=dataframe).fit()
+        Original_Loan_Term=smf.ols(formula='ORIGINAL_INTEREST_RATE~ORIGINAL_LOAN_TERM', data=dataframe).fit()
+        Number_of_borrowers=smf.ols(formula='ORIGINAL_INTEREST_RATE~NUMBER_OF_BORROWERS', data=dataframe).fit()
+        Seller_name=smf.ols(formula='ORIGINAL_INTEREST_RATE~SELLER_NAME', data=dataframe).fit()
+        Servicer_name=smf.ols(formula='ORIGINAL_INTEREST_RATE~SERVICER_NAME', data=dataframe).fit()
 
         
         # print the coefficients
@@ -69,30 +82,39 @@ def compute_coefficients(dataframe):
         
 
 #load the dataframe
-dataframe = pd.read_csv("sample.csv")
-
+dataframe = pd.read_csv("2005Q1.csv")
+dataframe_quarter = pd.read_csv("2005Q2.csv")
 #feature selection based on coefficient values
-feature_cols_dataframe=dataframe[["Property_state",'Property_type',"Loan_purpose",'Servicer_name','Original_interest_rate']]
-feature_cols=["Property_state",'Property_type',"Loan_purpose",'Servicer_name','Original_interest_rate']
+feature_cols_dataframe=dataframe[["PROPERTY_STATE",'PROPERTY_TYPE',"LOAN_PURPOSE",'SERVICER_NAME','ORIGINAL_INTEREST_RATE']]
+feature_cols=["PROPERTY_STATE",'PROPERTY_TYPE',"LOAN_PURPOSE",'SERVICER_NAME','ORIGINAL_INTEREST_RATE']
 
 #transforming categorical/string data into numeric data for the algorithm
 #used custom encoding instead of one hot encoding for memory efficiency
 transformed_df=dummyEncode(feature_cols_dataframe)
 print(transformed_df.head(5))
+transformed_df_next_quarter=dummyEncode(feature_cols_dataframe)
+print(transformed_df_next_quarter.head(5))
 #X = transformed_df
 #y = transformed_df.Original_interest_rate
 
 #split the data for training and testing 
-X_train = transformed_df.data[:-20]
-X_test  = transformed_df.data[-20:]
-Y_train = transformed_df.Original_interest_rate.target[:-20]
-Y_test  = transformed_df.Original_interest_rate.target[-20:]
+# X_train = transformed_df.data[:-20]
+# X_test  = transformed_df.data[-20:]
+# Y_train = transformed_df.Original_interest_rate.target[:-20]
+# Y_test  = transformed_df.Original_interest_rate.target[-20:]
+X_train=transformed_df
+Y_train=transformed_df.ORIGINAL_INTEREST_RATE
+
+X_test=transformed_df_next_quarter
+Y_test=transformed_df_next_quarter.ORIGINAL_INTEREST_RATE
 
 model_number = input("Choose the model that you want to run:"
                       "  A. Linear Regression "
                       "  B. Neural Network "
                       "  C. KNN"
-                      "  D. Random Forest")
+                      "  D. Random Forest"
+                      " "
+                     )
 
 if model_number == "A":
         #linear regression model
@@ -118,7 +140,7 @@ elif model_number == "B":
         X_test = scaler.transform(X_test)
 
         neural_network_reg=MLPClassifier(hidden_layer_sizes=(20,10,20))
-        neural_network_reg.fit(X_train,y_train)
+        neural_network_reg.fit(X_train,Y_train)
         predictions = neural_network_reg.predict(X_test)
         print("Intercept is ",neural_network_reg.intercept_)
         print("Coefficient is ",neural_network_reg.coef_)
@@ -130,10 +152,13 @@ elif model_number == "C":
         print("Starting KNN algorithm")
         for K in range(25):
                  K_value = K+1
-                 knn_reg = KNeighborsClassifier(n_neighbors = K_value, weights='uniform', algorithm='auto')
-                 knn_reg.fit(X_train, Y_train) 
+                 knn_reg = KNeighborsRegressor(n_neighbors = K_value, weights='uniform', algorithm='auto')
+                 knn_reg.fit(X_train, Y_train)
                  y_pred = knn_reg.predict(X_test)
-                 print "Accuracy is ", accuracy_score(Y_test,Y_train)*100,"% for K-Value:",K_value
+                 print("Mean Absolute Error is ",mean_absolute_error(Y_train,y_pred),"% for K-Value:",K_value)
+                 #print("Mean Absolute Percentage Error is ",mean_absolute_percentage_error(Y_train,y_pred))
+                 print("Root Mean Squared Error ",np.sqrt(mean_squared_error(Y_train,y_pred)))
+                 print("Accuracy of the model is ",r2_score(Y_train,y_pred))
 
 elif model_number == "D":
         print("Staring Random forest algorithm")
@@ -142,19 +167,8 @@ elif model_number == "D":
         random_forest.predict(X_test)
         #predict probability of first 10 records
         print(random_forest.predict_proba(X_test)[0:10])
-        print "Accuracy is ", accuracy_score(Y_test,Y_train)*100,"% for job:",K_value     
+        print ("Accuracy is ", accuracy_score(Y_test,Y_train)*100,"% for job:",n_jobs)     
         
 else:
         print("Running all four algorithms in parallel")
         
-
-"""
-# visualize the relationship between the features and the response using scatterplots
-fig, axs = plt.subplots(1, 6, sharey=True)
-dataframe.plot(kind='scatter', x='Credit_score', y='Original_interest_rate', ax=axs[0], figsize=(16, 8))
-dataframe.plot(kind='scatter', x='Original_combined_loan-to-value', y='Original_interest_rate', ax=axs[1])
-dataframe.plot(kind='scatter', x='Original_debt_to_income_ratio', y='Original_interest_rate', ax=axs[2])
-
-dataframe.plot(kind='scatter', x='Unpaid_principal_balance', y='Original_interest_rate', ax=axs[3])
-dataframe.plot(kind='scatter', x='Original-loan-to-value', y='Original_interest_rate', ax=axs[4])
-dataframe.plot(kind='scatter', x='Property_state', y='Original_interest_rate', ax=axs[5])"""
